b3f9581c903f5469510605e2045ff6de0ddeff45
dlexion <dlexion@yandex.ru>
2020-01-14 13:45:02 +0300

    Add rendering for pages

---
+++ EpiserverSite/EpiserverSite/Controllers/ArticlePageController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using EpiserverSite.Models.Pages;
+
+namespace EpiserverSite.Controllers
+{
+    public class ArticlePageController : PageController<ArticlePage>
+    {
+        public ActionResult Index(ArticlePage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
---
+++ EpiserverSite/EpiserverSite/Controllers/StartPageController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using EPiServer.Web.Mvc;
+using EpiserverSite.Models.Pages;
+
+namespace EpiserverSite.Controllers
+{
+    public class StartPageController : PageController<StartPage>
+    {
+        public ActionResult Index(StartPage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
