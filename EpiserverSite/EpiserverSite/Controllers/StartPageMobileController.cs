56eaebb9943df6e5407347a444402c4b64041105
dlexion <dlexion@yandex.ru>
2020-01-30 13:32:59 +0300

    Create Display Channel for mobile

--- EpiserverSite/EpiserverSite/Controllers/StartPageMobileController.cs
+++ EpiserverSite/EpiserverSite/Controllers/StartPageMobileController.cs
@@ -1,17 +1,18 @@
 ﻿using System.Web.Mvc;
 using EPiServer.Framework.DataAnnotations;
+using EPiServer.Framework.Web;
 using EPiServer.Web.Mvc;
 using EpiserverSite.Models.Pages;
 using EpiserverSite.Models.ViewModels;
 
 namespace EpiserverSite.Controllers
 {
-    [TemplateDescriptor(Default = true)]
-    public class StartPageController : PageController<StartPage>
+    [TemplateDescriptor(Tags = new string[] { RenderingTags.Mobile })]
+    public class StartPageMobileController : PageController<StartPage>
     {
         public ActionResult Index(StartPage currentPage)
         {
-            return View(new StartPageViewModel(currentPage));
+            return View("~/Views/StartPage/Index.mobile.cshtml", new StartPageViewModel(currentPage));
         }
     }
 }
