8787b26d64816cf21a613c16a7ad9040d2e67bfb
dlexion <dlexion@yandex.ru>
2020-03-30 20:41:46 +0300

    Create custom plugin and add button with episerver styles

---
+++ EpiserverSite/EpiserverSite/Business/Initialization/CustomRouteInitialization.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+
+namespace EpiserverSite.Business.Initialization
+{
+    [InitializableModule]
+    public class CustomRouteInitialization : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            RouteTable.Routes.MapRoute(
+                null,
+                "custom-plugins/my-plugin",
+                new { controller = "MyPlugin", action = "Index" });
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+    }
+}
