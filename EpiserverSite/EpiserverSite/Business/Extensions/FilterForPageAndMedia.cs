f09e682f19c407865bdd59aaba61b45ae29ffe4a
dlexion <dlexion@yandex.ru>
2020-04-02 20:53:41 +0300

    Add custom filter methods

---
+++ EpiserverSite/EpiserverSite/Business/Extensions/FilterForPageAndMedia.cs
@@ -0,0 +1,23 @@
+﻿using EPiServer.Cms.Shell;
+using EPiServer.Core;
+using EPiServer.Find;
+using EpiserverSite.Models.Pages;
+
+namespace EpiserverSite.Business.Extensions
+{
+    public static class FilterForPageAndMedia
+    {
+        public static ITypeSearch<TSource> FilterForPagesAndMedia<TSource>(this ITypeSearch<TSource> search, string language)
+            where TSource : class
+        {
+            var filterBuilder = new FilterBuilder<TSource>(search.Client);
+
+            filterBuilder = filterBuilder.And(x => (x.MatchTypeHierarchy(typeof(BasePage))
+                                                   & ((IContent)x).LanguageBranch().Match(language)));
+
+            filterBuilder = filterBuilder.Or(x => x.MatchTypeHierarchy(typeof(MediaData)));
+
+            return search.Filter(filterBuilder);
+        }
+    }
+}
--- EpiserverSite/EpiserverSite/Business/Services/FindService.cs
+++ EpiserverSite/EpiserverSite/Business/Services/FindService.cs
@@ -2,8 +2,8 @@
 using System.Linq;
 using EPiServer.Core;
 using EPiServer.Find;
-using EPiServer.Find.Cms;
 using EPiServer.Web.Routing;
+using EpiserverSite.Business.Extensions;
 using EpiserverSite.Models;
 
 namespace EpiserverSite.Business.Services
@@ -23,14 +23,15 @@ namespace EpiserverSite.Business.Services
         {
             var foundPages = _client.Search<IContent>()
                 .For(query)
-                //.InLanguageBranch(languageBranch) // can't find media. Media has Invariant Language (Invariant Country) language
-                .FilterForVisitor() // don't filter as FilterForVisitor.Filter method. Still have some unwanted results shown (recycle bin for example)
+                .CustomFilterForVisitor()
+                .FilterForPagesAndMedia(languageBranch)
                 .Take(maxResults)
-                .Select(x => new SearchResult()
-                {
-                    Name = x.Name,
-                    Url = _urlResolver.GetUrl(x.ContentLink),
-                })
+                .Select(x =>
+                    new SearchResult
+                    {
+                        Name = x.Name,
+                        Url = _urlResolver.GetUrl(x.ContentLink),
+                    })
                 .GetResult()
                 .Hits
                 .Select(x => x.Document)
