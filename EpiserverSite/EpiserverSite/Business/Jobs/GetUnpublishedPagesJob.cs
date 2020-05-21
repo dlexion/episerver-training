dbe16131da6099676a66c2a14a362dbc859b7d06
dlexion <dlexion@yandex.ru>
2020-05-21 10:32:22 +0300

    Update structure of resource file

--- EpiserverSite/EpiserverSite/Business/Jobs/GetUnpublishedPagesJob.cs
+++ EpiserverSite/EpiserverSite/Business/Jobs/GetUnpublishedPagesJob.cs
@@ -4,7 +4,6 @@ using System.Text;
 using EPiServer;
 using EPiServer.Core;
 using EPiServer.DataAbstraction;
-using EPiServer.Framework.Localization;
 using EPiServer.PlugIn;
 using EPiServer.Scheduler;
 using EpiserverSite.Models.Pages;
@@ -12,13 +11,13 @@ using EpiserverSite.Models.Pages;
 namespace EpiserverSite.Business.Jobs
 {
     [ScheduledPlugIn(
-        DisplayName = "displayname",
-        Description = "description",
+        DisplayName = "",
+        Description = "",
         DefaultEnabled = true,
         IntervalType = ScheduledIntervalType.Months,
         IntervalLength = 1,
         InitialTime = "2:0:0",
-        LanguagePath ="")]
+        LanguagePath = "/plugin/job/UnpublishedPages")]
     public class GetUnpublishedPagesJob : ScheduledJobBase
     {
         private readonly IContentLoader _contentLoader;
@@ -42,7 +41,7 @@ namespace EpiserverSite.Business.Jobs
             StringBuilder sb = new StringBuilder();
             sb.Append(unpublishedPages.Count).AppendLine(" unpublished page(s) was(were) found.");
 
-            if (unpublishedPages.Any())
+            if (unpublishedPages.Count > 0)
             {
                 sb.AppendLine("List of unpublished pages:");
                 foreach (var unpublishedPage in unpublishedPages)
