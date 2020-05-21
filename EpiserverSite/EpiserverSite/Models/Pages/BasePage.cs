669853b0cd991f8edeebae4a8b86e8119a31f2a6
dlexion <dlexion@yandex.ru>
2020-05-21 13:05:00 +0300

    Remove redundant overriding

--- EpiserverSite/EpiserverSite/Models/Pages/BasePage.cs
+++ EpiserverSite/EpiserverSite/Models/Pages/BasePage.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using EPiServer.Core;
-using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
 
 namespace EpiserverSite.Models.Pages
@@ -14,8 +13,5 @@ namespace EpiserverSite.Models.Pages
             GroupName = SystemTabNames.Content,
             Order = 10)]
         public virtual string Robots { get; set; }
-
-        [Display(GroupName = SystemTabNames.Content)]
-        public override CategoryList Category { get => base.Category; set => base.Category = value; }
     }
 }
